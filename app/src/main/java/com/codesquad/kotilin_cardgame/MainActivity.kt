49c6bfd9a1cb0f60a61446b16e48a92e9b75c772
ramkid91 <ramkid91@gmail.com>
2022-02-22 01:47:01 +0900

    refactor: divide nickname vertification function

--- app/src/main/java/com/codesquad/kotilin_cardgame/MainActivity.kt
+++ app/src/main/java/com/codesquad/kotilin_cardgame/MainActivity.kt
@@ -1,106 +1,157 @@
 package com.codesquad.kotilin_cardgame
 
 import android.app.AlertDialog
-import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.text.InputFilter
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_ENTER
-import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
-import com.google.android.material.snackbar.Snackbar
 import java.util.regex.Pattern
-import android.text.TextWatcher as TextWatcher
 
 class MainActivity : AppCompatActivity() {
+    var nicknameSelectedFlag = false
+    var profileImageSelectedFlag = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val editNickName = findViewById<EditText>(R.id.et_cadrgame_nickname)
-        val outerLayout = findViewById<LinearLayout>(R.id.linearly_out_card)
-        addNicknameVerification(editNickName)
+        val moveBtn = findViewById<Button>(R.id.btn_move_card)
         val buttonBigSmile = findViewById<ImageButton>(R.id.imgbtn_bigsmile_card)
         val buttonSmile = findViewById<ImageButton>(R.id.imgbtn_smile_card)
         val buttonSad = findViewById<ImageButton>(R.id.imgbtn_sad_card)
         val buttonBigSad = findViewById<ImageButton>(R.id.imgbtn_bigsad_card)
         val selectedImage = findViewById<ImageView>(R.id.iv_profile_card)
-        addImageButtonListner(buttonBigSmile, selectedImage)
-        addImageButtonListner(buttonSmile, selectedImage)
-        addImageButtonListner(buttonSad, selectedImage)
-        addImageButtonListner(buttonBigSad, selectedImage)
+        addTextFilter(editNickName)
+        addNicknameVerification(editNickName,moveBtn)
+        addImageButtonListener(buttonBigSmile, selectedImage, moveBtn)
+        addImageButtonListener(buttonSmile, selectedImage,moveBtn)
+        addImageButtonListener(buttonSad, selectedImage,moveBtn)
+        addImageButtonListener(buttonBigSad, selectedImage,moveBtn)
+        addLinkToSub(moveBtn)
 
     }
 
-    fun addImageButtonListner(button: ImageButton, selected: ImageView) {
+    fun addLinkToSub(moveBtn: Button) {
+        moveBtn.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun addImageButtonListener(button: ImageButton, selected: ImageView, link: Button) {
         button.setOnClickListener {
             when (it.id) {
-                R.id.imgbtn_bigsmile_card -> selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_very_satisfied_55, null))
-                R.id.imgbtn_smile_card -> selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_satisfied_55, null))
-                R.id.imgbtn_sad_card -> selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_dissatisfied_55, null))
-                R.id.imgbtn_bigsad_card -> selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_very_dissatisfied_24, null))
+                R.id.imgbtn_bigsmile_card -> {
+                    profileImageSelectedFlag = true
+                    if(nicknameSelectedFlag) link.isEnabled= true
+                    selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_very_satisfied_55, null))
+                }
+                R.id.imgbtn_smile_card -> {
+                    profileImageSelectedFlag = true
+                    if(nicknameSelectedFlag) link.isEnabled= true
+                    selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_satisfied_55, null)
+                    )
+                }
+                R.id.imgbtn_sad_card -> {
+                    profileImageSelectedFlag = true
+                    if(nicknameSelectedFlag) link.isEnabled= true
+                    selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_dissatisfied_55, null))
+                }
+                R.id.imgbtn_bigsad_card -> {
+                    profileImageSelectedFlag = true
+                    if(nicknameSelectedFlag) link.isEnabled= true
+                    selected.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_sentiment_very_dissatisfied_24, null))
+                }
                 else -> selected.setBackgroundColor(Color.WHITE)
             }
         }
     }
 
-    fun addNicknameVerification(editNickName:EditText){
-        editNickName.setOnKeyListener { v, keyCode, event ->
-            var handled = false
+    fun addTextFilter(editNickName: EditText) {
+        editNickName.setFilters(arrayOf(
+            InputFilter { src, start, end, dst, dstart, dend ->
+                if (src.matches(Regex("^[a-zA-Z0-9]+$"))) {
+                    return@InputFilter src
+                }
+                //backspace 입력시 처리
+                if (src == "") {
+                    return@InputFilter src
+                }
+                Toast.makeText(this, "영어 숫자만 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@InputFilter ""
+            }
+        ))
+    }
+
+    fun displayNickNameLengthError(editNickName: EditText) {
+        AlertDialog.Builder(this@MainActivity)
+            .setTitle("닉네임 길이 에러")
+            .setMessage("닉네임 길이는 1~5자 사이여야 합니다")
+            .setPositiveButton("ok", object : DialogInterface.OnClickListener {
+                override fun onClick(dialog: DialogInterface, which: Int) {
+                    editNickName.setText("")
+                    Toast.makeText(
+                        this@MainActivity,
+                        "닉네임 재입력 해주세여",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+            .show()
+    }
+
+    fun checkOnlyNumberType(editNickName: EditText, link: Button) {
+        val nickname = editNickName.text
+        val includeAlphaBet = Pattern.compile(".*[a-zA].*")
+        if (includeAlphaBet.matcher(nickname).matches()) {
+            Toast.makeText(
+                this@MainActivity,
+                "닉네임 입력 확인되었습니다",
+                Toast.LENGTH_SHORT
+            ).show()
+            nicknameSelectedFlag = true
+            if(profileImageSelectedFlag) link.isEnabled= true
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "알파벳이 하나라도 포함되도록 다시입력해주세요",
+                Toast.LENGTH_SHORT
+            ).show()
+            link.isEnabled=false
+            editNickName.setText("")
+        }
+    }
+
+    fun addNicknameVerification(editNickName: EditText , link: Button) {
+        editNickName.setOnKeyListener { view, keyCode, event ->
+            var handled= false
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) {
-                val nickname = editNickName.text.trim()
+                val nickname = editNickName.text
                 when {
                     nickname.length > 5 || nickname.isEmpty() -> {
-                        AlertDialog.Builder(this@MainActivity)
-                            .setTitle("닉네임 길이 에러")
-                            .setMessage("닉네임 길이는 1~5자 사이여야 합니다")
-                            .setPositiveButton("ok", object : DialogInterface.OnClickListener {
-                                override fun onClick(dialog: DialogInterface, which: Int) {
-                                    editNickName.setText("")
-                                    Toast.makeText(
-                                        this@MainActivity,
-                                        "닉네임 재입력 해주세여",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            })
-                            .show()
-                        handled = true
+                        displayNickNameLengthError(editNickName)
+                        link.isEnabled=false
+                        handled=true
                     }
                     else -> {
-                        val ps: Pattern =
-                            Pattern.compile("^[a-zA-Z0-9\\u318D\\u119E\\u11A2\\u2022\\u2025a\\u00B7\\uFE55]+$")
-                        if (ps.matcher(nickname).matches()) {
-
-                            val incluedeAlphaBet = Pattern.compile(".*[a-zA].*")
-                            if (incluedeAlphaBet.matcher(nickname).matches()) {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "닉네임 입력 확인되었습니다",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                handled = true
-                            } else {
-                                Toast.makeText(
-                                    this@MainActivity,
-                                    "알파벳이 하나라도 포함되도록 다시입력해주세요",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                editNickName.setText("")
-                            }
-
-                        } else {
-                            Toast.makeText(this, "영문, 숫자만 입력 가능합니다.", Toast.LENGTH_SHORT).show()
-                            editNickName.setText("")
-                        }
+                        checkOnlyNumberType(editNickName,link)
+                        handled=true
                     }
                 }
+            }
+            if (event.action == EditorInfo.IME_ACTION_DONE) {
+                // 키보드 내리기
+                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(editNickName.windowToken, 0)
+                handled=true
             }
             handled
         }
---
+++ app/src/main/java/com/codesquad/kotilin_cardgame/SubActivity.kt
@@ -0,0 +1,11 @@
+package com.codesquad.kotilin_cardgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
