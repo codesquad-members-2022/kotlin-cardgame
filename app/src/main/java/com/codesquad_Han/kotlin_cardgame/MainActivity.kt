891ea7fd1492db4da4381ff8140d101e8e3fb52b
hanchang97 <nimok97@gmail.com>
2022-02-21 18:53:28 +0900

    feat : 캐릭터 이미지 선택 기능 구현 4가지 캐릭터 이미지 선택 시 선택한 이미지 나타나도록 구현

--- app/src/main/java/com/codesquad_Han/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/codesquad_Han/kotlin_cardgame/MainActivity.kt
@@ -2,11 +2,14 @@ package com.codesquad_Han.kotlin_cardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import com.codesquad_Han.kotlin_cardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
 
+    fun selectCharacter(view: View) {
+        if (view is ImageView) {
+            binding.ivCharacterSelected.setImageDrawable(view.drawable)
+        }
     }
 }
