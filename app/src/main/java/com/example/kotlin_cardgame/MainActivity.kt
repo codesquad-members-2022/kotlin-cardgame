c5aa33654e86ef02188dda2d9dc9a4faf1e0a0f3
sim <shim506@naver.com>
2022-02-21 23:39:51 +0900

    FEAT :  캐릭터 ImageButton 구현
    - ImageView 의 크기를 정사각형으로 맞추기 위해서 ViewTreeObserver 를 사용
    - Vector 이미지 materialDesign 에서 받아와서 사용

--- app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.kotlin_cardgame
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import com.example.kotlin_cardgame.databinding.ActivityMainBinding
 
 private const val TAG = "MainActivity"
@@ -9,11 +13,31 @@ private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setImageButtonSize()
+
 
     }
+
+    private fun setImageButtonSize() {
+
+        val viewDrawingListener = object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                Log.d(TAG, binding.smileImageButton.width.toString())
+                binding.smileImageButton.layoutParams.height = binding.smileImageButton.width
+                binding.smileImageButton.requestLayout()
+                binding.smileImageButton.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        }
+
+        binding.smileImageButton.viewTreeObserver.addOnGlobalLayoutListener (viewDrawingListener )
+
+    }
+
+
 }
