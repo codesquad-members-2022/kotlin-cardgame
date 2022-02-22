ece3c0f757350d27e2f51c3539516abcec6854cb
ramkid91 <ramkid91@gmail.com>
2022-02-22 14:58:09 +0900

    feat: make subActivty's toolBar and fragment xml

--- app/src/main/java/com/codesquad/kotilin_cardgame/SubActivity.kt
+++ app/src/main/java/com/codesquad/kotilin_cardgame/SubActivity.kt
@@ -3,6 +3,7 @@ package com.codesquad.kotilin_cardgame
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,10 @@ class SubActivity : AppCompatActivity() {
         val profileImage = intent.getByteArrayExtra("profileImage")!!.let{
             BitmapFactory.decodeByteArray(it,0,it.size)
         }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_cardgame)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
 
 
     }
