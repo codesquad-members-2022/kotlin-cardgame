db08de7d61330fbb037103ddabaac197e79b59e8
banjjak2 <interr0bang@naver.com>
2022-02-22 13:44:35 +0900

    feat: 캐릭터 버튼 클릭 리스너 등록
    캐릭터 버튼 클릭 시 이전 버튼의 배경은 원래대로 변경해야 하고 현재 클릭된 버튼은 클릭 배경으로 변경하기 위함

--- app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
@@ -2,14 +2,33 @@ package com.example.kotlin_cardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import com.example.kotlin_cardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var prevSelectBtn: ImageButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with (binding) {
+            btnCharacter1.setOnClickListener(clickCharacterButtonListener)
+            btnCharacter2.setOnClickListener(clickCharacterButtonListener)
+            btnCharacter3.setOnClickListener(clickCharacterButtonListener)
+            btnCharacter4.setOnClickListener(clickCharacterButtonListener)
+        }
+    }
+
+    private val clickCharacterButtonListener = View.OnClickListener {
+        (it as? ImageButton)?.apply {
+            prevSelectBtn?.apply { this.setBackgroundResource(R.color.character_btn_normal_bg) }
+            prevSelectBtn = this
+            binding.ivSelectCharacter.setImageDrawable(this.drawable)
+            this.setBackgroundResource(R.color.character_btn_select_bg)
+        }
     }
 }
