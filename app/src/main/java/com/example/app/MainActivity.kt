e6f44f090a758c74a0074f7a7a7f9921d998b79e
name <junseoks9306@gmail.com>
2022-02-22 15:18:20 +0900

    feat : button 선택시 button 이미지 변화

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,22 +2,31 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import com.example.app.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val layout = findViewById<LinearLayout>(R.id.main_layout)
+        val editTextButton = findViewById<EditText>(R.id.character_name_input_button)
+        val button1 = findViewById<ImageButton>(R.id.character_button1)
+        val button2 = findViewById<ImageButton>(R.id.character_button2)
+        val button3 = findViewById<ImageButton>(R.id.character_button3)
+        val button4 = findViewById<ImageButton>(R.id.character_button4)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
     }
 
     override fun onStart() {
         super.onStart()
-        binding.textViewActivity.text = "iam Josh"
+
+
     }
 }
