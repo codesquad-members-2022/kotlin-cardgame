ed0fe42b310df18b11c65785089ff48f4b9c97f9
ese111 <feung94@gmail.com>
2022-02-23 00:58:32 +0900

    feat: 도움말 버튼에 링크걸기 구현

--- app/src/main/java/com/example/kotlincardgame/OptionFragment.kt
+++ app/src/main/java/com/example/kotlincardgame/OptionFragment.kt
@@ -1,13 +1,17 @@
 package com.example.kotlincardgame
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+
 
 class OptionFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,12 @@ class OptionFragment : Fragment() {
         val drawable: ImageView =
             activity?.findViewById(R.id.iv_character_info) ?: throw NullPointerException()
         checkUser(nickname, textView, imageView, drawable)
+        val button: Button =
+            view?.findViewById(R.id.help) ?: throw NullPointerException()
+        button.setOnClickListener {
+            val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://codesquad.kr/"))
+            startActivity(urlIntent)
+        }
         return view
     }
 }
