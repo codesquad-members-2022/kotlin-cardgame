583a35242bdbc262e34c647b9d7eaf8337208e10
sim <shim506@naver.com>
2022-02-22 14:35:09 +0900

    FEAT : 버튼 클릭에대한 이미지 나타내기
    - 최초 이미지버튼 disable 화
    - 이미지 버튼 클릭시 이미지 나타내기
    - 이미지 그린후 잘못된 닉네임 입력시 이미지 지우기

--- app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.kotlin_cardgame
 
 import android.app.ActionBar
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.addTextChangedListener
 import com.example.kotlin_cardgame.databinding.ActivityMainBinding
 
@@ -21,11 +24,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setImageButtonSize()
+        disableImageButton()
 
+        nicknameEditTextListening()
+
+        emotionViewsClickListening()
+
+
+    }
+
+    private fun disableImageButton() {
+        binding.smileImageButton.isEnabled = false
+        binding.neutralImageButton.isEnabled = false
+        binding.dissatisfiedImageButton.isEnabled = false
+        binding.badImageButton.isEnabled = false
+    }
+
+    private fun emotionViewsClickListening() {
+
+        binding.smileImageButton.setOnClickListener { binding.emotionImageView.setImageResource(R.drawable.baseline_mood_24) }
+        binding.neutralImageButton.setOnClickListener { binding.emotionImageView.setImageResource(R.drawable.baseline_sentiment_neutral_24) }
+        binding.dissatisfiedImageButton.setOnClickListener {
+            binding.emotionImageView.setImageResource(
+                R.drawable.baseline_sentiment_very_dissatisfied_24
+            )
+        }
+        binding.badImageButton.setOnClickListener { binding.emotionImageView.setImageResource(R.drawable.baseline_mood_bad_24) }
+
+    }
+
+    private fun nicknameEditTextListening() {
         binding.nicknameEditText.addTextChangedListener {
+            // 버튼 활성화 및 비활성화
             val isClickable: Boolean = isNicknameAble(binding.nicknameEditText.text.toString())
             setEmotionButtonEnable(isClickable)
 
+            // 이모션 클릭상태에서도 잘못된 글자 입력시 이미지 없애기
+            if (!isClickable) binding.emotionImageView.setImageBitmap(null)
         }
     }
 
