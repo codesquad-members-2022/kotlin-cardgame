88d202f499220869a9619c0fd1ec60d5c8844ba2
ese111 <feung94@gmail.com>
2022-02-22 19:12:18 +0900

    feat: 화면 구현과 데이터 바인딩

--- app/src/main/java/com/example/kotlincardgame/MainActivity.kt
+++ app/src/main/java/com/example/kotlincardgame/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.kotlincardgame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlincardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view.rootView)
+        
     }
+
 }
