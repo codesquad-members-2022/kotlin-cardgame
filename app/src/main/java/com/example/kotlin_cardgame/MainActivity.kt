b0ad1254a2081555768d8984fd537b0ec481d036
banjjak2 <interr0bang@naver.com>
2022-02-22 15:05:08 +0900

    refact: verifyInputValue에서 with구문 추가
    binding 입력없이 접근할 수 있도록 함

--- app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_cardgame/MainActivity.kt
@@ -33,15 +33,15 @@ class MainActivity : AppCompatActivity() {
     private val verifyInputValue = object: TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
-        override fun afterTextChanged(p0: Editable?) {
+        override fun afterTextChanged(p0: Editable?) = with(binding) {
             if (p0.toString().matches(Regex("^(?!(?:[0-9]+)\$)([a-zA-Z]|[0-9a-zA-Z])+\$"))) {
-                binding.tvInputVerify.setTextColor(Color.BLUE)
-                binding.tvInputVerify.text = "사용가능한 닉네임입니다."
-                binding.btnNext.isEnabled = true
+                tvInputVerify.setTextColor(Color.BLUE)
+                tvInputVerify.text = "사용가능한 닉네임입니다."
+                btnNext.isEnabled = true
             } else {
-                binding.tvInputVerify.setTextColor(Color.RED)
-                binding.tvInputVerify.text = "알파벳이 1개 이상 포함되어야 합니다. (공백, 특수문자 제외)"
-                binding.btnNext.isEnabled = false
+                tvInputVerify.setTextColor(Color.RED)
+                tvInputVerify.text = "알파벳이 1개 이상 포함되어야 합니다. (공백, 특수문자 제외)"
+                btnNext.isEnabled = false
             }
         }
     }
