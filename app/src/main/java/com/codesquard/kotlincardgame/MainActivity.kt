b4177f55944c3d00ffe0815034c7f23cdcd4b98d
Linus <zerolecture0922@gmail.com>
2022-02-22 11:57:13 +0900

    Fetch : 버튼의 선택 상태에 대한 코드

--- app/src/main/java/com/codesquard/kotlincardgame/MainActivity.kt
+++ app/src/main/java/com/codesquard/kotlincardgame/MainActivity.kt
@@ -6,26 +6,66 @@ import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.core.graphics.drawable.toDrawable
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.android.material.textfield.TextInputLayout.*
-import java.util.*
-import java.util.regex.Pattern
-import kotlin.math.E
 
 class MainActivity : AppCompatActivity() {
     private lateinit var nextActivityBtn: Button
     private lateinit var nicknameText: TextInputEditText
+    private lateinit var firstBtn: ImageButton
+    private lateinit var secondBtn: ImageButton
+    private lateinit var thirdBtn: ImageButton
+    private lateinit var fourthBtn: ImageButton
+    private lateinit var characterImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         nextActivityBtn = findViewById(R.id.btn_nextActivity)
         nicknameText = findViewById(R.id.nickname_text)
+        firstBtn = findViewById(R.id.first_btn)
+        secondBtn = findViewById(R.id.second_btn)
+        thirdBtn = findViewById(R.id.third_btn)
+        fourthBtn = findViewById(R.id.fourth_btn)
+        characterImage = findViewById(R.id.character_image)
         setNicknameEditTextLayout()
         setNicknameEditText()
         checkNicknameText()
+        clickBtnToShowImage()
+    }
+
+    private fun setBtnNotSelected() {
+        firstBtn.isSelected = false
+        secondBtn.isSelected = false
+        thirdBtn.isSelected = false
+        fourthBtn.isSelected = false
+    }
+
+    private fun clickBtnToShowImage() {
+        firstBtn.setOnClickListener {
+            setBtnNotSelected()
+            characterImage.setImageDrawable(getDrawable(R.drawable.first))
+            firstBtn.isSelected = true
+        }
+        secondBtn.setOnClickListener {
+            setBtnNotSelected()
+            characterImage.setImageDrawable(getDrawable(R.drawable.second))
+            secondBtn.isSelected = true
+        }
+        thirdBtn.setOnClickListener {
+            setBtnNotSelected()
+            characterImage.setImageDrawable(getDrawable(R.drawable.third))
+            thirdBtn.isSelected = true
+        }
+        fourthBtn.setOnClickListener {
+            setBtnNotSelected()
+            characterImage.setImageResource(R.drawable.fourth)
+            fourthBtn.isSelected = true
+        }
     }
 
     private fun checkNicknameText() {
@@ -34,12 +74,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if ((p0?.length ?: 0) == 0) nextActivityBtn.isEnabled = false
             }
 
             override fun afterTextChanged(p0: Editable?) {
                 p0?.filter {
                     nextActivityBtn.isEnabled = true
-                    if (!(p0.contains("[A-Z|a-z]".toRegex()))) nextActivityBtn.isEnabled = false
+                    if ((!(p0.contains("[A-Z|a-z]".toRegex())))) nextActivityBtn.isEnabled = false
                     !(it.isLetterOrDigit())
                 }?.forEach {
                     nextActivityBtn.isEnabled = false
@@ -62,5 +103,4 @@ class MainActivity : AppCompatActivity() {
         nicknameText.filters = arrayOf(InputFilter.LengthFilter(5))
         nicknameText.setSingleLine()
     }
-
 }
