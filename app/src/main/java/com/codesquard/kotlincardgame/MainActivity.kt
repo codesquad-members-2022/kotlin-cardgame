acdb95f5db542ad48f39ae1fa3b4ee3111a38e6d
Linus <zerolecture0922@gmail.com>
2022-02-22 10:52:27 +0900

    Fetch : EditText 속성 코드로 설정하고 닉네임 체크 후 버튼 비활성화 구현 EditText와 TextInputLay 속성을 활용해 제약 조건 및 디자인 변경 addTextChangedListener 콜백함수로 EditText에 입력된 값이 바뀔 때마다 이벤트 처리

--- app/src/main/java/com/codesquard/kotlincardgame/MainActivity.kt
+++ app/src/main/java/com/codesquard/kotlincardgame/MainActivity.kt
@@ -2,10 +2,65 @@ package com.codesquard.kotlincardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textfield.TextInputLayout.*
+import java.util.*
+import java.util.regex.Pattern
+import kotlin.math.E
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nextActivityBtn: Button
+    private lateinit var nicknameText: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nextActivityBtn = findViewById(R.id.btn_nextActivity)
+        nicknameText = findViewById(R.id.nickname_text)
+        setNicknameEditTextLayout()
+        setNicknameEditText()
+        checkNicknameText()
     }
+
+    private fun checkNicknameText() {
+        nicknameText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                p0?.filter {
+                    nextActivityBtn.isEnabled = true
+                    if (!(p0.contains("[A-Z|a-z]".toRegex()))) nextActivityBtn.isEnabled = false
+                    !(it.isLetterOrDigit())
+                }?.forEach {
+                    nextActivityBtn.isEnabled = false
+                } ?: throw IllegalArgumentException("잘못된 값입니다")
+            }
+        })
+    }
+
+    private fun setNicknameEditTextLayout() {
+        val nicknameTextLayout: TextInputLayout = findViewById(R.id.nickname_text_layout)
+        nicknameTextLayout.endIconMode = END_ICON_CLEAR_TEXT
+        nicknameTextLayout.helperText = "최대 5글자, 공백/특수문자 제외, 최소 1글자는 알파벳 포함"
+        nicknameTextLayout.isCounterEnabled = true
+        nicknameTextLayout.counterMaxLength = 5
+        nicknameTextLayout.hint = "닉네임"
+    }
+
+    private fun setNicknameEditText() {
+        val nicknameText: TextInputEditText = findViewById(R.id.nickname_text)
+        nicknameText.filters = arrayOf(InputFilter.LengthFilter(5))
+        nicknameText.setSingleLine()
+    }
+
 }
