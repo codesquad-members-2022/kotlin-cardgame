1ebd83d89ac62f2e0c3cada447788c3f54c5167b
hanchang97 <nimok97@gmail.com>
2022-02-23 20:26:47 +0900

    feat : 닉네임 작명 조건 추가 최대5글자, 알파벳 최소 한 개 포함, 특수문자와 공백 미포함 조건 구현

--- app/src/main/java/com/codesquad_Han/kotlin_cardgame/MainActivity.kt
+++ app/src/main/java/com/codesquad_Han/kotlin_cardgame/MainActivity.kt
@@ -3,16 +3,22 @@ package com.codesquad_Han.kotlin_cardgame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import com.codesquad_Han.kotlin_cardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val characterArray = arrayOfNulls<Pair<View, Boolean>>(4)
+
+    private var isAlphabetIncluded = false
+    private var isSpeciaCharactersNotIncluded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         setCharacterArray()
         setCharacterClickListener()
+        setEditText()
 
         binding.btnNext.setOnClickListener {
             startActivity(Intent(this, GameActivity::class.java))
@@ -54,5 +61,41 @@ class MainActivity : AppCompatActivity() {
         }
         characterArray[selected]!!.first.setBackgroundColor(ContextCompat.getColor(this, R.color.selected))
         characterArray[selected] = characterArray[selected]!!.copy(second = true)
+    }
+
+    fun setEditText(){
+        binding.etNickName.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                checkNickName(s)
+            }
+        })
+    }
+
+    fun checkNickName(s: Editable?){
+        isAlphabetIncluded = false
+        isSpeciaCharactersNotIncluded = true
+        var nickname = s.toString()
+        for(i in 0..nickname.length-1){
+            if(nickname[i] in 'a'..'z' || nickname[i] in 'A'..'Z'){
+                isAlphabetIncluded = true
+            }
+            if(!((nickname[i] in '0'..'9')||(nickname[i] in 'a'..'z')||(nickname[i] in 'A'..'Z'))){
+                isSpeciaCharactersNotIncluded = false
+                break
+            }
+        }
+        checkBtn()
+    }
+
+    fun checkBtn(){
+        binding.btnNext.isEnabled = isAlphabetIncluded && isSpeciaCharactersNotIncluded
     }
 }
