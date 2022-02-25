3eb262f18231bcf00416ab6d38df454056fe8b80
liyusang1 <liyusang1@naver.com>
2022-02-25 13:46:57 +0900

    FEAT : 카드덱 기능 코드 추가

--- app/src/main/java/com/example/cardgame/Card.kt
+++ app/src/main/java/com/example/cardgame/Card.kt
@@ -1,6 +1,9 @@
 package com.example.cardgame
 
-class Card {
+class Card(private var cardCount: Int) : CardInterface {
+
+    var cardDeck = mutableListOf<String>()
+
     private fun getRandomShape(): String {
         return when ((1..4).random()) {
             1 -> Shape.APPLE.shape
@@ -10,11 +13,17 @@ class Card {
         }
     }
 
+    private fun initCard() {
+        repeat(cardCount) {
+            cardDeck.add(returnCard())
+        }
+    }
+
     private fun getRandomNumber(): Int {
         return (1..10).random()
     }
 
-    fun returnCard(): String {
+    private fun returnCard(): String {
         val number: String
         val temp = getRandomNumber()
         number = when (temp) {
@@ -26,11 +35,43 @@ class Card {
         val shape: String = getRandomShape()
         return shape + number
     }
+
+    override fun count(): Int {
+        return cardDeck.size
+    }
+
+    override fun shuffle() {
+        var i = cardDeck.size
+        while (i > 1) {
+            i -= 1
+            var j = (1..i).random()
+            val temp = cardDeck[j]
+            cardDeck[j] = cardDeck[i]
+            cardDeck[i] = temp
+        }
+    }
+
+    override fun removeOne(): String {
+        if (cardDeck.isEmpty()) {
+            return "남은 카드가 더 이상 없습니다."
+        }
+        val card = cardDeck[cardDeck.size - 1]
+        cardDeck.removeAt(cardDeck.size - 1)
+        cardCount -= 1
+        println("총 ${cardCount}개의 카드가 남아있습니다")
+        return card
+    }
+
+    override fun reset() {
+        initCard()
+        println("카드를 초기화 했습니다. ${cardCount}개의 카드가 있습니다.")
+    }
 }
 
 fun main() {
-    val card = Card()
+    val card = Card(10)
+    card.reset()
     for (i in 0..10) {
-        println(card.returnCard())
+        println(card.removeOne())
     }
 }
---
+++ app/src/main/java/com/example/cardgame/CardInterface.kt
@@ -0,0 +1,8 @@
+package com.example.cardgame
+
+interface CardInterface {
+    fun count() : Int
+    fun shuffle()
+    fun removeOne() :String
+    fun reset()
+}
